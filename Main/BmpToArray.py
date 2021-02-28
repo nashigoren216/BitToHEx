b9e0f1037050506b50fa358f26e8f319a6ab0dd7
Arata Kobayashi <xiaolinxin.332@gmail.com>
2021-02-28 23:01:54 +0900

    必要ファイルの追加

--- Main/BmpToArray.py
+++ Main/BmpToArray.py
@@ -4,50 +4,6 @@ import * = まとめてimport
 from 
 """
 
-class ExampleWidget(QMainWindow):
-    
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):      
-
-        self.textEdit = QTextEdit()
-        self.setCentralWidget(self.textEdit)
-        self.statusBar()
-
-        # メニューバーのアイコン設定
-        openFile = QAction(QIcon('imoyokan.jpg'), 'Open', self)
-        # ショートカット設定
-        openFile.setShortcut('Ctrl+O')
-        # ステータスバー設定
-        openFile.setStatusTip('Open new File')
-        openFile.triggered.connect(self.showDialog)
-
-        # メニューバー作成
-        menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&File')
-        fileMenu.addAction(openFile)       
-
-        self.setGeometry(300, 300, 350, 300)
-        self.setWindowTitle('File dialog')
-        self.show()
-
-
-    def showDialog(self):
-
-        # 第二引数はダイアログのタイトル、第三引数は表示するパス
-        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')
-        # fname[0]は選択したファイルのパス（ファイル名を含む）
-        if fname[0]:
-            # ファイル読み込み
-            f = open(fname[0], 'r')
-
-            # テキストエディタにファイル内容書き込み
-            with f:
-                data = f.read()
-                self.textEdit.setText(data)
-
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
--- Main/MainWindows.py
+++ Main/MainWindows.py
@@ -4,3 +4,53 @@ import sys
 from PyQt5.QtWidgets import (QMainWindow, QTextEdit, 
     QAction, QFileDialog, QApplication)
 from PyQt5.QtGui import QIcon
+from bmpread import *
+
+
+
+class ExampleWidget(QMainWindow):
+    
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):      
+
+        self.textEdit = QTextEdit()
+        self.setCentralWidget(self.textEdit)
+        self.statusBar()
+
+        # メニューバーのアイコン設定
+        openFile = QAction(QIcon('1513411692.png'), 'Open', self)
+        # ショートカット設定
+        openFile.setShortcut('Ctrl+O')
+        # ステータスバー設定
+        openFile.setStatusTip('Open new File')
+        openFile.triggered.connect(self.showDialog)
+
+        # メニューバー作成
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(openFile)       
+
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle('File dialog')
+        self.show()
+
+
+    def showDialog(self):
+
+        # 第二引数はダイアログのタイトル、第三引数は表示するパス
+        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')
+        # fname[0]は選択したファイルのパス（ファイル名を含む）
+        if fname[0]:
+            # ファイル読み込み
+            f = open(fname[0], 'r')
+
+            # テキストエディタにファイル内容書き込み
+            BmpRead(f)
+            """
+                with f:
+                data = f.read()
+                self.textEdit.setText(data)
+            """
